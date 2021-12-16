d1b7d96c73a0971b8750acdeb8974355249fa987
mariodavid <mario@die-davids.de>
2021-12-16 07:57:15 +0100

    fix tests

--- jmix-wizard/src/test/java/de/diedavids/jmix/wizard/WizardTest.java
+++ jmix-wizard/src/test/java/de/diedavids/jmix/wizard/WizardTest.java
@@ -1,13 +1,9 @@
 package de.diedavids.jmix.wizard;
 
-import de.diedavids.jmix.wizard.entity.Foo;
 import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class WizardTest {
@@ -19,15 +15,4 @@ class WizardTest {
 	void contextLoads() {
 	}
 
-	@Test
-	void testFoo() {
-		Foo foo = dataManager.create(Foo.class);
-		foo.setName("abc");
-
-		Foo foo1 = dataManager.save(foo);
-		assertEquals(foo, foo1);
-
-		Foo foo2 = dataManager.load(Id.of(foo)).one();
-		assertEquals(foo, foo2);
-	}
 }
