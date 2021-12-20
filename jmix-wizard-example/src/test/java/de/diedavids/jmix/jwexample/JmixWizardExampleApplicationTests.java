c3a178efe24deda540f6106ede0fabb307ac6760
mariodavid <mario@die-davids.de>
2021-12-20 08:08:08 +0100

    deactivate example app tests

--- jmix-wizard-example/src/test/java/de/diedavids/jmix/jwexample/JmixWizardExampleApplicationTests.java
+++ jmix-wizard-example/src/test/java/de/diedavids/jmix/jwexample/JmixWizardExampleApplicationTests.java
@@ -1,5 +1,6 @@
 package de.diedavids.jmix.jwexample;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JmixWizardExampleApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
